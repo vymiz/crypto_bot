83895925cfbb4733891436ff508863fcf9048739
VMZ <vymiz020777@gmail.com>
2021-10-12 22:01:39 +0300

    crypto_bot edited

--- ini.py
+++ ini.py
@@ -1,7 +1,7 @@
 TOKEN = '2079435695:AAG_zPb1e33KRHN0eh97Uwm6J0USdp58VZY'
 
 keys ={
-    'биток':'BTC',
-    'эфир':'ETH',
-    'бакс':'USD'
+    'рубль':'RUB',
+    'евро':'EUR',
+    'доллар':'USD'
 }
